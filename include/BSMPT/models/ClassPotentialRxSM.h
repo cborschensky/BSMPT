3b1870fbecd02515f20620f388aaad92cc6da1c1
Christoph Borschensky <christoph.borschensky@kit.edu>
2026-02-26 22:05:31 +0100

    Implemented RxSM model

---
+++ include/BSMPT/models/ClassPotentialRxSM.h
@@ -0,0 +1,101 @@
+// Copyright (C) 2018  Philipp Basler and Margarete Mühlleitner
+// SPDX-FileCopyrightText: 2021 Philipp Basler, Margarete Mühlleitner and Jonas
+// Müller
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+/**
+ * @file
+ * Model file for the SM + real singlet with the tree-level potential
+ */
+
+#pragma once
+
+#include <string> // for string
+#include <vector> // for vector
+
+#include <BSMPT/models/ClassPotentialOrigin.h>
+using namespace Eigen;
+
+namespace BSMPT
+{
+namespace Models
+{
+
+/**
+ * @brief The Class_Potential_RxSM class
+ * Lagrangian from https://arxiv.org/pdf/1512.05355 Eq. (11)
+ */
+class Class_Potential_RxSM : public Class_Potential_Origin
+{
+public:
+  Class_Potential_RxSM(const ISMConstants &smConstants);
+  virtual ~Class_Potential_RxSM();
+
+  // Choice of parameters of Lagrangian from https://arxiv.org/pdf/1512.05355
+  // Eq. (11)
+  double lambdaS, lambdaHS, vS;
+
+  // Not an input parameter; lambda is fixed via the requirement of having
+  // one of the Higgs bosons as the SM one with 125.09 GeV
+  double lambda;
+
+  // Not an input parameter; set to the SM value
+  double vH;
+
+  // Not input parameters; set through the tadpole equations
+  double msq, mSsq;
+
+  double alpha;
+
+  bool UnbrokenSingletPhase;
+
+  double dmsq, dlambda, dmSsq, dlambdaS, dlambdaHS, dT1, dT2, dT3, dT4, dT5;
+
+  std::size_t pos_Gp, pos_Gm, pos_G0, pos_h, pos_H;
+  std::size_t pos_h_SM, pos_h_H;
+
+  void ReadAndSet(const std::string &linestr,
+                  std::vector<double> &par) override;
+  std::vector<std::string> addLegendCT() const override;
+  std::vector<std::string> addLegendTemp() const override;
+  std::vector<std::string> addLegendTripleCouplings() const override;
+  std::vector<std::string> addLegendVEV() const override;
+
+  void set_gen(const std::vector<double> &par) override;
+  void set_CT_Pot_Par(const std::vector<double> &par) override;
+  void write() const override;
+
+  /*
+   * RxSM interaction basis:
+   * 0     1     2     3      4
+   * rho1, eta1, psi1, zeta1, zetaS
+   */
+  const std::size_t pos_rho1 = 0, pos_eta1 = 1, pos_psi1 = 2, pos_zeta1 = 3,
+                    pos_zetaS = 4;
+
+  /**
+   * Helper function to determine mass indices of rotation matrix
+   * @param HiggsMasses : vector with squared Higgs masses allocated
+   *                      in AdjustRotationMatrix
+   * @param HiggsRot : rotation matrix from interaction to mass basis
+   *                   as calculated in AdjustRotationMatrix
+   */
+  void FindMassBasisIndices(const std::vector<double> &HiggsMasses,
+                            const MatrixXd &HiggsRot);
+
+  void AdjustRotationMatrix() override;
+  void TripleHiggsCouplings() override;
+  std::vector<double> calc_CT() const override;
+
+  void SetCurvatureArrays() override;
+  bool CalculateDebyeSimplified() override;
+  bool CalculateDebyeGaugeSimplified() override;
+  double VTreeSimplified(const std::vector<double> &v) const override;
+  double VCounterSimplified(const std::vector<double> &v) const override;
+  void Debugging(const std::vector<double> &input,
+                 std::vector<double> &output) const override;
+};
+
+} // namespace Models
+} // namespace BSMPT
