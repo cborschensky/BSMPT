5f744b824427a4986589ce5b6af34ff7956a4429
Christoph Borschensky <christoph.borschensky@gmail.com>
2026-03-06 11:44:56 +0100

    Added more documentation to RxSM header file

--- include/BSMPT/models/ClassPotentialRxSM.h
+++ include/BSMPT/models/ClassPotentialRxSM.h
@@ -24,7 +24,49 @@ namespace Models
 
 /**
  * @brief The Class_Potential_RxSM class
- * Lagrangian from https://arxiv.org/pdf/1512.05355 Eq. (11)
+ * Implementation of the RxSM according to https://arxiv.org/pdf/1512.05355 Eq.
+ * (11)
+ *
+ * * \f$ -L_S = (msq / 2) H^\dagger H + (\lambda / 4) (H^\dagger H)^2 +
+ * (\lambda_{HS} / 2) (H^\dagger H) S^2 + (mSsq / 2) S^2 + (\lambda_S / 24) S^4
+ * \f$
+ * * \f$ -L_Y = \overline{U}_L V \text{diag}(y_d,y_s,y_b) D_R H^+  +
+ * \overline{D}_L \text{diag}(y_d,y_s,y_b) D_R H^0 + \overline{U}_L
+ * \text{diag}(y_u, y_c, y_t) U_R (H^0)^\ast - \overline{D}_L V^\dagger
+ * \text{diag}(y_u, y_c, y_t) U_R H^- + \overline{E}_L \text{diag}(y_e, y_\mu,
+ * y_\tau) E_R H^0 +\overline{\nu}_L \text{diag}(y_e, y_\mu, y_\tau) E_R H^+ +
+ * c.c.  \f$
+ * * \f$ L_G = (D_\mu H)^\dagger (D^\mu H) \f$
+ *
+ * with
+ *
+ * * \f$ H = \begin{pmatrix} H^+ \\  H^0 \end{pmatrix} = 1/\sqrt{2}
+ * \begin{pmatrix} higgsfield[0] + I*higgsfield[1] \\  higgsfield[3] + I
+ * *higgsfield[2] \end{pmatrix}\,, \f$
+ * * \f$ S = higgsfield[4] \f$ with higgsfield = [rho1,eta1,psi1,zeta1,zetaS]
+ * * \f$ D_\mu = -I C\_{}g/2 * W_\mu^a \sigma^a -I C\_{}gs/2 B_\mu =
+ * -\frac{I}{2} \begin{pmatrix} C\_{}gs B + C\_{}g W3  & C\_{}g (W1 -I W2) \\
+ * C\_{}g (W1 +I W2) & C\_{}gs B - C\_{}g W3 \end{pmatrix} \f$ with the gauge
+ * base = [W1,W2,W3,B]
+ *
+ * * \f$ \overline{U}_L = \begin{pmatrix} \overline{u}_L &  \overline{c}_L &
+ * \overline{t}_L \end{pmatrix}\,, \f$
+ * * \f$ U_R = \begin{pmatrix} u_R \\ c_R \\ t_R \end{pmatrix}\,, \f$
+ * * \f$ \overline{D}_L = \begin{pmatrix} \overline{d}_L & \overline{s}_L &
+ * \overline{b}_L \end{pmatrix}\,, \f$
+ * * \f$ D_R = \begin{pmatrix} d_R \\ s_R \\ b_R \end{pmatrix}\,, \f$
+ * * \f$ \overline{E}_L = \begin{pmatrix} \overline{e}_L & \overline{\mu}_L &
+ * \overline{\tau}_L \end{pmatrix}\,, \f$
+ * * \f$ E_R = \begin{pmatrix} e_R \\ \mu_R \\ \tau_R \end{pmatrix}\,, \f$
+ * * \f$ \overline{\nu}_L = \begin{pmatrix} \overline{\nu}_{e,L} &
+ * \overline{\nu}_{\mu,L} & \overline{\nu}_{\tau,L} \end{pmatrix} \f$
+ *
+ * The basis of the quarks given by \f$ [u_R, c_R, t_R, d_R, s_R, b_R,
+ * \overline{u}_L, \overline{c}_L, \overline{t}_L, \overline{d}_L,
+ * \overline{s}_L, \overline{b}_L ] \f$ and for the leptons \f$ [\overline{e}_L,
+ * e_R, \overline{\mu}_L , \mu_R, \overline{\tau}_L, \tau_R,
+ * \overline{\nu}_{e,L}, \overline{\nu}_{\mu,L}, \overline{\nu}_{\tau,L} ] \f$
+ *
  */
 class Class_Potential_RxSM : public Class_Potential_Origin
 {
